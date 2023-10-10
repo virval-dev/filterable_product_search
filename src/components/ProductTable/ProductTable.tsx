5e7b96236f624da6cb2a99817878a6418dd8710a
vivaldev <valtteri.virtanen.dev@gmail.com>
2023-10-10 10:26:30 +0300

    SearchBarComponent and search condition selects

---
+++ src/components/ProductTable/ProductTable.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProductTableComponent: React.FC = () => {
+  // code
+
+  return (
+    <div className="product-table-component">
+      <h5 className="product-table-text">Name</h5>
+      <h5 className="product-table-text">Price</h5>
+      <h5 className="product-table-text">Quantity</h5>
+    </div>
+  );
+};
+
+export default ProductTableComponent;
